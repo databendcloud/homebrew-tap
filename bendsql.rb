90d7b121f4c8b23cc5e5c418e5d4fd122a3b0947
hantmac <hantmac@outlook.com>
2022-10-06 22:57:39 +0800

    add bendsql.rb

---
+++ bendsql.rb
@@ -0,0 +1,21 @@
+class Bendsql < Formula
+  desc "Work seamlessly with Databend Cloud from the command-line."
+  homepage "https://github.com/databendcloud/bendsql"
+  url "https://github.com/databendcloud/bendsql/releases/download/v0.0.2/bendsql-darwin-amd64.tar.gz"
+  sha256 "25c1a2a4e1922261535325634a939fe42a0ffcc12ae6c262ed7021dab611f622"
+  license "Apache-2.0"
+  version "v0.0.2"
+  head "https://github.com/databendcloud/bendsql.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPROXY"] = "https://goproxy.io"
+    system "make install"
+    bin.install "bendsql"
+  end
+
+  test do
+    system "false"
+  end
+end
