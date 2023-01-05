b26de4ee49bf5642d87de116e487967835c390be
Jeremy <hantmac@outlook.com>
2023-01-05 16:20:16 +0800

    Update bendsql.rb
    Upgrade bendsql to 0.1.2

--- bendsql.rb
+++ bendsql.rb
@@ -1,10 +1,10 @@
 class Bendsql < Formula
   desc "Work seamlessly with Databend Cloud from the command-line."
   homepage "https://github.com/databendcloud/bendsql"
-  url "https://github.com/databendcloud/bendsql/releases/download/v0.0.6/bendsql-darwin-amd64.tar.gz"
-  sha256 "371b8b27f38013442c1b368f9d667609dc5502959a976976124339763d1fa5e9"
+  url "https://github.com/databendcloud/bendsql/releases/download/v0.1.2/bendsql-darwin-amd64.tar.gz"
+  sha256 "9b9b1b174e502547fdedfaa2af401d60592b1da9feb7e8a6221d3c130643f135"
   license "Apache-2.0"
-  version "v0.0.6"
+  version "v0.1.2"
   head "https://github.com/databendcloud/bendsql.git", branch: "main"
 
   depends_on "go" => :build
