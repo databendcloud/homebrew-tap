4b9884e8f49cf1e6bc7daeed9548fc18233ee442
everpcpc <everpcpc@users.noreply.github.com>
2023-04-23 18:22:43 +0800

    feat: upgrade bendsql to v0.3.0

--- bendsql.rb
+++ bendsql.rb
@@ -1,8 +1,8 @@
 class Bendsql < Formula
   desc "Databend Native Command-Line Tool"
   homepage "https://databend.rs"
-  url "https://github.com/datafuselabs/databend-client/archive/v0.2.6.tar.gz"
-  sha256 "83ac732f7b4d75751a2f52f02d3acc4fb50e4dea13ae1be40fa41239e9c7d50b"
+  url "https://github.com/datafuselabs/databend-client/archive/v0.3.0.tar.gz"
+  sha256 "632ae1e76f52dc8401d401974feff155101a7559fb21901fd658df510b6e6c4e"
   license "Apache-2.0"
 
   depends_on "rust" => :build
