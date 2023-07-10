6a36d682331dd5d89e169c85f774412295d0d8de
everpcpc <everpcpc@users.noreply.github.com>
2023-07-10 18:17:50 +0800

    chore: upgrade to v0.4.0

--- bendsql.rb
+++ bendsql.rb
@@ -1,8 +1,8 @@
 class Bendsql < Formula
   desc "Databend Native Command-Line Tool"
   homepage "https://databend.rs"
-  url "https://github.com/datafuselabs/databend-client/archive/v0.3.10.tar.gz"
-  sha256 "0b36031848c18c597c1fe3bdc098fb4ed78a72075085eb1ac9430746fbd260d6"
+  url "https://github.com/datafuselabs/databend-client/archive/v0.4.0.tar.gz"
+  sha256 "087a37f4e80597c12afc6a0149c3ad28157ef98df0d0dfd4530e4b799b566fad"
   license "Apache-2.0"
 
   depends_on "rust" => :build
@@ -16,6 +16,6 @@ class Bendsql < Formula
 
   test do
     output = shell_output("#{bin}/bendsql -V")
-    assert_match "bendsql 0.3.10\n", output
+    assert_match "bendsql 0.4.0\n", output
   end
 end
