fbd725c956c5b19b55e3da4ad2e8a13568cc4227
everpcpc <git@everpcpc.com>
2023-09-08 16:03:35 +0800

    z

--- bendsql.rb
+++ bendsql.rb
@@ -1,7 +1,7 @@
 class Bendsql < Formula
   desc "Databend Native Command-Line Tool"
   homepage "https://databend.rs"
-  url "https://github.com/datafuselabs/databend-client/archive/v0.6.0.tar.gz"
+  url "https://github.com/datafuselabs/databend-client/archive/v0.6.1.tar.gz"
   sha256 "63bbe328fd6c5c69e3c6567192779e77f6fe088b1c8f6d8e4b74670fa8ed99ba"
   license "Apache-2.0"
 
