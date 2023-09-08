e78a4737bbb0f3ac3e1480408778e41421f4831e
everpcpc <git@everpcpc.com>
2023-09-08 18:21:15 +0800

    chore: upgrade to v0.6.5
    Closes #5.
    
    Signed-off-by: everpcpc <1808802+everpcpc@users.noreply.github.com>

--- bendsql.rb
+++ bendsql.rb
@@ -1,8 +1,8 @@
 class Bendsql < Formula
   desc "Databend Native Command-Line Tool"
   homepage "https://databend.rs"
-  url "https://github.com/datafuselabs/databend-client/archive/v0.6.3.tar.gz"
-  sha256 "3230c2a6a500738d89364b3990665c634878bb2e34caf0c76c6ebd15139fa42b"
+  url "https://github.com/datafuselabs/databend-client/archive/v0.6.5.tar.gz"
+  sha256 "da43d6f554774886c1b8ffda3b0a55d46dc4ca9f6521e3124eb3eec10096b623"
   license "Apache-2.0"
 
   bottle do
@@ -21,6 +21,6 @@ class Bendsql < Formula
 
   test do
     output = shell_output("#{bin}/bendsql -V")
-    assert_match "bendsql 0.6.3-dev()\n", output
+    assert_match "bendsql 0.6.5-dev()\n", output
   end
 end
