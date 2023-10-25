4b71c19d27f0efbc6e74fa4d49d48884d6105850
everpcpc <everpcpc@users.noreply.github.com>
2023-10-25 11:49:32 +0800

    chore: update 0.9.2 bottle

--- bendsql.rb
+++ bendsql.rb
@@ -5,11 +5,6 @@ class Bendsql < Formula
   sha256 "aa69e81287d267a8960d04e4259c1a203c34b0e96378f18b4d8d239219576e97"
   license "Apache-2.0"
 
-  bottle do
-    root_url "https://github.com/databendcloud/homebrew-tap/releases/download/bendsql-0.9.1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "340e5a59897b3b1e4a2c1ce3a1c7d8dfe57c2a43f18875360ba3bf1695fb8ff9"
-  end
-
   depends_on "rust" => :build
 
   def install
