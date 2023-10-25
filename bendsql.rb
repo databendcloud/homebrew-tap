3241d47594060130690d3841a649986ca50f13eb
everpcpc <everpcpc@users.noreply.github.com>
2023-10-25 12:21:12 +0800

    fix: update release url
    Closes #12.
    
    Signed-off-by: everpcpc <1808802+everpcpc@users.noreply.github.com>

--- bendsql.rb
+++ bendsql.rb
@@ -1,7 +1,7 @@
 class Bendsql < Formula
   desc "Databend Native Command-Line Tool"
   homepage "https://databend.rs"
-  url "https://github.com/datafuselabs/databend-client/archive/v0.9.2.tar.gz"
+  url "https://github.com/datafuselabs/bendsql/archive/refs/tags/v0.9.2.tar.gz"
   sha256 "aa69e81287d267a8960d04e4259c1a203c34b0e96378f18b4d8d239219576e97"
   license "Apache-2.0"
 
