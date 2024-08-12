8ee30eebf130c889d2c28dc4113993e8254ab218
everpcpc <git@everpcpc.com>
2024-08-12 12:31:37 +0800

    feat(bendsql): upgrade to 0.20.1

--- bendsql.rb
+++ bendsql.rb
@@ -1,12 +1,12 @@
 class Bendsql < Formula
   desc "Databend Native Command-Line Tool"
   homepage "https://databend.rs"
-  url "https://github.com/datafuselabs/bendsql/archive/refs/tags/v0.18.2.tar.gz"
-  sha256 "67b163255c848f54b81ffcd8e9cd8380f32003737c2d536141d5506a9a30c50a"
+  url "https://github.com/datafuselabs/bendsql/archive/refs/tags/v0.20.1.tar.gz"
+  sha256 "8fd2bdaabf592930a136fa32194dffa52288a1c4b140e91ccbad421c72192ab0"
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/databendcloud/homebrew-tap/releases/download/bendsql-0.18.2"
+    root_url "https://github.com/databendcloud/homebrew-tap/releases/download/bendsql-0.20.1"
     sha256 cellar: :any_skip_relocation, arm64_sonoma: "53261bfe8d578d9a7670246d05128d226d773e42ca15779a30d7060e3be4d66e"
   end
 
